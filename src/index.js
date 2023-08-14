19c5e66ae7f26eca03595e947bc92c8ec494306a
Mantik0re <elzakv18@gmail.com>
2023-08-14 16:04:35 +0300

    feat: a dding a check

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+
     let revStr = '';
+    n = n.toString();
     for (let i = n.length - 1; i >= 0; i--) {
-        revStr += n[i];
+        if (n[0] != 0 && n[i] != '-') {
+            revStr += n[i];
+        }
+        
     }
     return revStr;
 }
